cee5fb43d0d8c90353d702623834fb62ed6ce5d8
Damir Sarajlic <damirdesigned@gmail.com>
2019-03-30 14:53:00 +1100

    chore:prettified syntax

--- summing-obj.js
+++ summing-obj.js
@@ -1,22 +1,22 @@
-    var obj = {
-      a: 1,
-      b: 2,
-      c: 3
-    };
+var obj = {
+  a: 1,
+  b: 2,
+  c: 3
+};
 
-    function sum1(obj) {
-      var sum = 0;
-      for (var el in obj) {
-        if (obj.hasOwnProperty(el)) {
-          sum += parseFloat(obj[el]);
-        }
-      }
-      return sum;
+function sum1(obj) {
+  var sum = 0;
+  for (var el in obj) {
+    if (obj.hasOwnProperty(el)) {
+      sum += parseFloat(obj[el]);
     }
+  }
+  return sum; 
+}
 
-    function sum2(obj) {
-      return Object.keys(obj)
-        .reduce(function (sum, key) {
-          return sum + parseFloat(obj[key]);
-        }, 0);
-    }
+function sum2(obj) {
+  return Object.keys(obj)
+    .reduce(function (sum, key) {
+      return sum + parseFloat(obj[key]);
+    }, 0);
+}
